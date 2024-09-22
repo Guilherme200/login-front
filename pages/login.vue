ba3fd33b03b09bd4fbde035775457faf86400830
Guilherme200 <guimatoso15@gmail.com>
2024-09-22 00:30:53 -0300

    Adicionar validações de formulários

--- pages/login.vue
+++ pages/login.vue
@@ -3,16 +3,38 @@
     <div class="card bg-base-100 w-96 shadow-xl">
       <div class="card-body">
         <h2 class="flex justify-center text-3xl py-5">Login</h2>
-        <input type="text" placeholder="username" class="input input-bordered w-full max-w-xs" />
-        <input type="password" placeholder="password" class="input input-bordered w-full max-w-xs" />
-        <div class="card-actions justify-center py-5">
-          <button class="btn btn-primary btn w-full">Login</button>
-        </div>
+        <Form @submit="onSubmit" :validation-schema="validationSchema">
+          <div class="form-control">
+            <Field name="email" placeholder="username" class="input input-bordered w-full max-w-xs"/>
+            <ErrorMessage class="text-red-500" name="email"/>
+          </div>
+
+          <div class="form-control">
+            <Field name="password" type="password" placeholder="password" class="input input-bordered w-full max-w-xs"/>
+            <ErrorMessage class="text-red-500" name="password"/>
+          </div>
+          <div class="card-actions justify-center py-5">
+            <button type="submit" class="btn btn-primary btn w-full">Login</button>
+          </div>
+        </Form>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-console.log('Login')
+<script setup>
+import * as zod from 'zod';
+import {toTypedSchema} from '@vee-validate/zod';
+import {Form, Field, ErrorMessage} from 'vee-validate';
+
+const validationSchema = toTypedSchema(
+    zod.object({
+      email: zod.string().min(1, { message: 'Email é obrigatório' }).email({ message: 'Email inválido' }),
+      password: zod.string().min(1, { message: 'Senha é obrigatório' }).min(8, { message: 'Senha curta' }),
+    })
+);
+
+function onSubmit(values) {
+  alert(JSON.stringify(values, null, 2));
+}
 </script>
