943e9846fd7470ab3fbe52d3a4dff60cfad4117d
Guilherme200 <guimatoso15@gmail.com>
2024-09-22 00:22:33 -0300

    Adicionar layout da tela de login

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Login</h1>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div class="card bg-base-100 w-96 shadow-xl">
+      <div class="card-body">
+        <h2 class="flex justify-center text-3xl py-5">Login</h2>
+        <input type="text" placeholder="username" class="input input-bordered w-full max-w-xs" />
+        <input type="password" placeholder="password" class="input input-bordered w-full max-w-xs" />
+        <div class="card-actions justify-center py-5">
+          <button class="btn btn-primary btn w-full">Login</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+
+          "--rounded-box": "1rem",
+          "--rounded-btn": "0.5rem",
+          "--rounded-badge": "1.9rem",
+          "--animation-btn": "0.25s",
+          "--animation-input": "0.2s",
+          "--btn-focus-scale": "0.95",
+          "--border-btn": "1px",
+          "--tab-border": "1px",
+          "--tab-radius": "0.5rem",
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+}
+
