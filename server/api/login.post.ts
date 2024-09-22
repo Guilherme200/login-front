329188b6f57f26c6a97a90a2627801d3b48bda4c
Guilherme200 <guimatoso15@gmail.com>
2024-09-22 00:58:38 -0300

    Ajustar endpoint de login com supabase

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,6 +1,15 @@
-export default defineEventHandler(async () => {
-    return {
-        message: 'OK',
-        statusCode: 200
-    }
+export default defineEventHandler(async (event) => {
+    const data = await readBody(event);
+
+    const apiKey = process.env['SUPABASE_KEY'];
+    const baseUrl = process.env['SUPABASE_URL'];
+
+    return await $fetch(`${baseUrl}/auth/v1/token?grant_type=password`, {
+        method: 'POST',
+        headers: {
+            'apikey': apiKey,
+            'Content-Type': 'application/json',
+        },
+        body: data
+    })
 })
